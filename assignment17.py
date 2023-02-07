1a85049110dafb5d8119d074a8854c6496a9ff06
sgrobe875 <sgrobe@uvm.edu>
2023-02-07 10:46:03 -0500

    initial creation of file and roadmap

---
+++ assignment17.py
@@ -0,0 +1,26 @@
+# POCS HW 17
+# Due 2/10/23
+
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+# Basic methodology: function which takes in n
+# Use n to determine the bounds of the hexagon (n on each side)
+# --> maybe find the center and work back from there?
+# --> find the vertices of the hexagon
+# working up from the bottom, determine algorithm for number of points in each row
+# space that number of points evenly (np.linspace?)
+# to build the adjacency list, use networkx! 
+# assign coordinates to each node/point 
+# build the adjacency list
+# --> neighbor = any node within distance 1 of the given node (this might be tricky)
+# --> for now/to get the required output, assume connection b/w all neighbors
+
+
+
+
+
