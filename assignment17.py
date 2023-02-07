f3e7bae52886677bcd0df6494ba00db70150250b
sgrobe875 <sgrobe@uvm.edu>
2023-02-07 12:26:27 -0500

    plots all points in the lattice

--- assignment17.py
+++ assignment17.py
@@ -24,3 +24,152 @@ import numpy as np
 
 
 
+
+
+
+### FUNCTIONIZE AT THE END ####
+
+n = 8
+
+
+
+# Function starts here
+
+radius = n-1
+half_radius = radius/2
+
+# assume the center is the origin (0,0)
+
+# start with finding vertices (start at lower left and go counter-clockwise)
+v1 = {'x': -1 * half_radius, 
+      'y': -1 * radius}
+v2 = {'x': half_radius, 
+      'y': -1 * radius}
+v3 = {'x': radius, 
+      'y': 0}
+v4 = {'x': half_radius, 
+      'y': radius}
+v5 = {'x': -1 * half_radius, 
+      'y': radius}
+v6 = {'x': -1 * radius, 
+      'y': 0}
+
+# put in a list to help with graphing
+vertices = [v1, v2, v3, v4, v5, v6]
+
+
+start_x = v1['x']
+start_y = v1['y']
+end_x = v2['x']
+end_y = v2['y']
+
+# build dictionary to hold non-vertex points
+# key = tuple of (x,y) coordinates of the point
+# value = useful info like adjacency list (empty list for now)
+
+points = dict()
+
+
+# starting with the bottom half of the hexagon
+x = start_x
+y = start_y
+
+
+while y != 0:
+    x = start_x
+    key = str(x) + ',' + str(y)
+    points[key] = []
+    
+    while x != end_x:
+        x += 1
+        key = str(x) + ',' + str(y)
+        points[key] = []
+        
+    y += 1
+    start_x -= 0.5
+    end_x += 0.5
+
+
+# repeat for the top half
+start_x = v5['x']
+start_y = v5['y']
+end_x = v4['x']
+end_y = v4['y']
+
+x = start_x
+y = start_y
+
+while y != 0:
+    x = start_x
+    key = str(x) + ',' + str(y)
+    points[key] = []
+    
+    while x != end_x:
+        x += 1
+        key = str(x) + ',' + str(y)
+        points[key] = []
+        
+    y -= 1
+    start_x -= 0.5
+    end_x += 0.5
+
+
+
+# do the middle row
+start_x = v6['x']
+start_y = v6['y']
+end_x = v3['x']
+end_y = v3['y']
+
+x = start_x
+y = start_y
+
+key = str(x) + ',' + str(y)
+points[key] = []
+
+while x != end_x:
+    x += 1
+    key = str(x) + ',' + str(y)
+    points[key] = []
+    
+start_x -= 0.5
+end_x += 0.5
+
+
+
+
+
+
+
+
+
+
+# finally, plot
+
+# start with vertices
+for v in vertices:   
+    plt.scatter(v['x'], v['y'], color = 'blue')
+    
+# now plot the contents of the points dict
+x = []
+y = []
+for point in list(points.keys()):
+    values = point.split(',')
+    plt.scatter(float(values[0]), float(values[1]), color = 'red')
+    
+    
+    
+    
+    
+    
+    
+    
+plt.show()
+
+
+
+
+
+
+
+
